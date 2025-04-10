5e41e1a8a218341eb43aba7dbbfda0e6f43edc08
boy-hack <34109680@qq.com>
2025-04-11 01:57:14 +0800

    优化代码

--- pkg/core/conf/config.go
+++ pkg/core/conf/config.go
@@ -1,7 +1,7 @@
 package conf
 
 const (
-	Version     = "1.9.9"
+	Version     = "2.0"
 	AppName     = "KSubdomain"
 	Description = "无状态子域名爆破工具"
 )
---
+++ pkg/device/device.go
@@ -0,0 +1,56 @@
+package device
+
+import (
+	"net"
+	"os"
+	"time"
+
+	"github.com/boy-hack/ksubdomain/pkg/core/gologger"
+	"github.com/google/gopacket/pcap"
+	"gopkg.in/yaml.v3"
+)
+
+// EtherTable 存储网卡信息的数据结构
+type EtherTable struct {
+	SrcIp  net.IP  `yaml:"src_ip"`  // 源IP地址
+	Device string  `yaml:"device"`  // 网卡设备名称
+	SrcMac SelfMac `yaml:"src_mac"` // 源MAC地址
+	DstMac SelfMac `yaml:"dst_mac"` // 目标MAC地址（通常是网关）
+}
+
+// ReadConfig 从文件读取EtherTable配置
+func ReadConfig(filename string) (*EtherTable, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var ether EtherTable
+	err = yaml.Unmarshal(data, &ether)
+	if err != nil {
+		return nil, err
+	}
+	return &ether, nil
+}
+
+// SaveConfig 保存EtherTable配置到文件
+func (e *EtherTable) SaveConfig(filename string) error {
+	data, err := yaml.Marshal(e)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, data, 0666)
+}
+
+// PcapInit 初始化pcap句柄
+func PcapInit(devicename string) (*pcap.Handle, error) {
+	var (
+		snapshot_len int32         = 1024
+		timeout      time.Duration = -1 * time.Second
+	)
+	handle, err := pcap.OpenLive(devicename, snapshot_len, false, timeout)
+	if err != nil {
+		gologger.Fatalf("pcap初始化失败:%s\n", err.Error())
+		return nil, err
+	}
+	return handle, nil
+}
